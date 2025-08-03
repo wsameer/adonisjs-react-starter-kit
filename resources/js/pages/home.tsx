e37d8138ee76867fa1917bfcbc3c99632d272145
wsameer <dev.wsameer@gmail.com>
2025-08-03 10:31:01 -0400

    sets up initial config for typescript

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -338,7 +338,7 @@ export default function Home() {
 
         <div className="text-sm text-center [&>code]:font-medium [&>code]:text-[#a599ff] bg-sand-12 text-sand-1 fixed bottom-0 left-0 right-0 py-2">
           Route for this page is registered in <code>start/routes.ts</code> file, rendering{' '}
-          <code>inertia/pages/home.tsx</code> template
+          <code>resources/js/pages/home.tsx</code> template
         </div>
       </div>
     </>
