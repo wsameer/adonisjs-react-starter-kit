684aef399c5919cf19668996f5d5896cb9a7c4a0
wsameer <dev.wsameer@gmail.com>
2025-08-03 10:56:39 -0400

    setup shadcn and tailwind 4.1

--- resources/js/pages/home.tsx
+++ resources/js/pages/home.tsx
@@ -1,9 +1,12 @@
+import { Button } from '@/components/ui/button'
 import { Head } from '@inertiajs/react'
 
 export default function Home() {
   return (
     <>
       <Head title="Homepage" />
+
+      <Button>Hello</Button>
 
       <div className="fixed xl:absolute left-8 right-8 top-0 bottom-0 xl:inset-0 max-w-screen-xl mx-auto before:content-[''] before:[background:repeating-linear-gradient(0deg,var(--sand-5)_0_4px,transparent_0_8px)] before:absolute before:top-0 before:left-0 before:h-full before:w-px after:content-[''] after:[background:repeating-linear-gradient(0deg,var(--sand-5)_0_4px,transparent_0_8px)] after:absolute after:top-0 after:right-0 after:h-full after:w-px"></div>
 
--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,15 @@ import { getDirname } from '@adonisjs/core/helpers'
 import inertia from '@adonisjs/inertia/client'
 import react from '@vitejs/plugin-react'
 import adonisjs from '@adonisjs/vite/client'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [inertia({ ssr: { enabled: false } }), react(), adonisjs({ entrypoints: ['resources/js/app/app.tsx'], reload: ['resources/views/**/*.edge'] })],
+  plugins: [
+    inertia({ ssr: { enabled: false } }),
+    react(),
+    adonisjs({ entrypoints: ['resources/js/app/app.tsx'], reload: ['resources/views/**/*.edge'] }),
+    tailwindcss()
+  ],
 
   /**
    * Define aliases for importing modules from
