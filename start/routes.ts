1328f1de5795e7b9f99fe92b275293fd39fab0ec
wsameer <dev.wsameer@gmail.com>
2025-08-06 22:32:00 -0400

    updates user model and add routes for authentication

--- start/routes.ts
+++ start/routes.ts
@@ -7,24 +7,35 @@
 |
 */
 
-const RegistersController = () => import('#controllers/registers_controller')
 const SessionController = () => import('#controllers/session_controller')
 import router from '@adonisjs/core/services/router'
 import { middleware } from './kernel.js'
+const RegisteredUsersController = () => import('#controllers/registered_users_controller')
 
 // Web routes
 router.on('/').renderInertia('welcome').as('home')
-router.on('/login').renderInertia('auth/login')
-router.on('/register').renderInertia('auth/register')
+
+// Web routes
+router.group(() => {
+  router.get('/login', [SessionController, 'create']).as('login')
+  router.get('/register', [RegisteredUsersController, 'create']).as('register')
+})
 
 // API routes
 // router.any('/api/*', [TrpcController, 'handle']).as('api')
 router
   .group(() => {
     router.post('/login', [SessionController, 'store']).as('login')
-    router.delete('/logout', [SessionController, 'destroy']).as('logout')
-    router.post('/register', [RegistersController, 'store']).as('register_user')
+    router.post('/register', [RegisteredUsersController, 'store']).as('register_user')
   })
   .use([middleware.guest()])
   .as('auth-routes')
   .prefix('api/auth')
+
+router.on('dashboard').renderInertia('dashboard').use(middleware.auth()).as('dashboard')
+
+router
+  .group(() => {
+    router.post('/logout', [SessionController, 'destroy']).as('logout')
+  })
+  .use([middleware.auth()])
